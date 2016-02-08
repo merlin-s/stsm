d48938a9b9c61cf773799adad5b953ccd32e8d5c
Merlin Schindlbeck <me+github@lunarstrain.de>
2016-02-08 22:54:03 +0100

    add n4428 support to enums

--- util/include/util/enum.h
+++ util/include/util/enum.h
@@ -1,3 +1,4 @@
+/** author: Merlin Schindlbeck */
 #ifndef UTIL_ENUM_H_INCLUDED
 #define UTIL_ENUM_H_INCLUDED
 
@@ -10,6 +11,7 @@
 
 #include <boost/preprocessor/repetition.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/reverse.hpp>
 #include <boost/preprocessor/seq/size.hpp>
 #include <boost/preprocessor/stringize.hpp>
 #include <boost/preprocessor/variadic/to_seq.hpp>
@@ -17,12 +19,34 @@
 #include <array>
 #include <map>
 
-#define my_enum_s (val1)(val2)(val3)
+#ifndef ENUM_N4428_NS
+#define ENUM_N4428_NS std
+#endif
+namespace ENUM_N4428_NS {
+template <class Enum> struct enum_traits {
+  struct enumerators { /*
+     static constexpr size_t const size;
+     template <size_t I> struct get {
+       static constexpr char const * const  identifier;
+       static constexpr Enum const value;
+     };*/
+  };
+};
+}
 
 #define UTIL_seq_gen_ec(r, data, elem) elem,
 #define UTIL_seq_gen_scoped_elem(r, data, elem) data::elem,
 #define UTIL_seq_gen_enum_pair(r, data, elem)                                  \
   { data::elem, BOOST_PP_STRINGIZE(elem) }
+
+#define UTIL_seq_gen_traits_enumerator_get(r, data, elem)                      \
+  template <>                                                                  \
+  template <>                                                                  \
+  struct enum_traits<data>::enumerators::get<BOOST_PP_SUB(r, 2)> {             \
+    static constexpr char const *const identifier = BOOST_PP_STRINGIZE(elem);  \
+    static constexpr const data value = data::elem;                          \
+  };
+
 #define UTIL_seq_gen_enum_pair_c(r, data, elem)                                \
   UTIL_seq_gen_enum_pair(r, data, elem),
 #define UTIL_eval_call(func, ...) func(__VA_ARGS__)
@@ -39,6 +63,18 @@
       BOOST_PP_SEQ_FOR_EACH(UTIL_seq_gen_enum_pair_c, name, seq)};             \
   const std::array<name, BOOST_PP_SEQ_SIZE(seq)> name##_array = {              \
       BOOST_PP_SEQ_FOR_EACH(UTIL_seq_gen_scoped_elem, name, seq)};
+
+#define UTIL_ENUM_N4428_FROM_SEQ(name, seq)                                    \
+  namespace ENUM_N4428_NS {                                                    \
+  template <> struct enum_traits<name>::enumerators {                          \
+    static const constexpr size_t size = BOOST_PP_SEQ_SIZE(seq);               \
+    template <size_t I> struct get {};                                         \
+  };                                                                           \
+  BOOST_PP_SEQ_FOR_EACH(UTIL_seq_gen_traits_enumerator_get, name, seq)         \
+  }
+
+#define UTIL_ENUM_N4428(name, ...)                                             \
+  UTIL_ENUM_N4428_FROM_SEQ(name, BOOST_PP_TUPLE_TO_SEQ((__VA_ARGS__)))
 
 #define UTIL_ENUM(name, ...)                                                   \
   UTIL_ENUM_FROM_SEQ(name, BOOST_PP_TUPLE_TO_SEQ((__VA_ARGS__)))
